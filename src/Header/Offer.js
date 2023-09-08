05b57061d4f7cd79f27182df68a91125dd5f2e89
mohammed muneesh E K <mohammedmuneesh1@gmail.com>
2023-09-08 15:01:58 +0530

    run build success

--- src/Header/Offer.js
+++ src/Header/Offer.js
@@ -11,7 +11,7 @@ const Offer = () => {
   return (
     <div className="offer-head">
       <span>
-        Extra 7.5% off ICICI Bank Cards | Offer Extended |{" "}
+        Extra 7.5% off ICICI Bank Cards | Offer Extended |
         <a href="https://www.pepperfry.com/">Hurry & shop now! </a>
       </span>
       <span className="material-symbols-outlined close" onClick={close}>
