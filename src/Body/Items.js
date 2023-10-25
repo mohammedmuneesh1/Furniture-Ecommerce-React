161c31bac6c188f1217fa96965a24510c189d382
mohammed muneesh E K <mohammedmuneesh1@gmail.com>
2023-10-25 10:43:58 +0530

    Card design Updated

--- src/Body/Items.js
+++ src/Body/Items.js
@@ -5,89 +5,72 @@ import {
   MDBCard,
   MDBCardBody,
   MDBCardImage,
-  MDBRipple,
+  MDBCardTitle, // Add import for MDBCardTitle
 } from "mdb-react-ui-kit";
+
 import { MyData } from "../Main-Component/MyData";
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
-const Items = () => {
-  const navigate=useNavigate();
-  const {item}=useContext(MyData);
-  const disP=item.slice(0,8); //to display first 8 products only
 
+const Items = () => {
+  const navigate = useNavigate();
+  const { item } = useContext(MyData);
+  const disP = item.slice(0, 8); // to display the first 8 products only
 
   const handlePrice = (price) => {
-    const formattedPrice = Number(price).toLocaleString('en-IN'); // 'en-IN' for Indian numbering system (e.g., 1,23,456.00)
-    return '₹' + formattedPrice;
+    const formattedPrice = Number(price).toLocaleString("en-IN"); // 'en-IN' for the Indian numbering system (e.g., 1,23,456.00)
+    return "₹" + formattedPrice;
   };
-
 
   return (
     <>
-      <MDBContainer fluid className="my-5 text-center">
-        <h4 className="mt-4 mb-5">
+      <section className="products d-flex flex-column align-items-center">
+        <h1 className="mt-5 text-black fw-bolder">
           <strong>Bestsellers</strong>
-        </h4>
+        </h1>
 
-        <MDBRow>
-
-
-  {disP.map((value,index) => (
-    
-    <MDBCol xl="3" lg="4" md="6" sm="6" xs="12" className="mb-4"   key={value.id}>
-    <MDBCard className=" card-size m-auto"   key={value.id}   onClick={()=>navigate(`/Product/${value.id}`)} >
-        <MDBRipple
-          rippleColor="light"
-          rippleTag="div"
-          className="bg-image rounded hover-zoom"
-        
-        >
-          <div className="image-container">
-            <MDBCardImage
-              src={value.src}
-              fluid
-              className="w-100 custom-image"
-              alt="Product"
-            />
-          </div>
-          <span>
-            <div className="mask">
-              <div className="d-flex justify-content-start align-items-end h-100">
-                <h5>
-                  <span className="badge bg-primary ms-2">New</span>
-                </h5>
-              </div>
-            </div>
-            <div className="hover-overlay">
-              <div
-                className="mask"
-                style={{ backgroundColor: "rgba(251, 251, 251, 0.15)" }}
-              ></div>
-            </div>
-            </span>
-        </MDBRipple>
-        <MDBCardBody className="custom-card-body p-1 p-md-3 p-lg-4">
-          <span  className="text-reset">
-            <h5 className="card-title mb-2 mb-md-3 mb-lg-3 h5-responsive">
-            {value.name}
-            </h5>
-          </span>
-          <span className="text-reset">
-            <p className="mb-2 mb-lg-4 text-muted card-category " >{value.category}</p>
-          </span>
-          <h6 className="mb-2 card-price">{handlePrice(value.price)}</h6>
-        </MDBCardBody>
-      </MDBCard>
-
-    </MDBCol>
-
-
-
-  ))}
-
-        </MDBRow>
-      </MDBContainer>
+        <MDBContainer fluid className="my-5">
+          <MDBRow className="best-seller d-flex flex-wrap justify-content-center mx-5">
+            {disP.map((value, index) => (
+              <MDBCol
+                xl="3"
+                lg="4"
+                md="4"
+                sm="6"
+                className="mb-3 best-seller-image"
+                key={value.id}
+                onClick={() => navigate(`/Product/${value.id}`)} // Fix the onClick here
+              >
+                <MDBCard className="text-black">
+                  <MDBCardImage
+                    src={value.src}
+                    position="top"
+                    alt="product-image"
+                  />
+                  <MDBCardBody>
+                    <div className=" text-center">
+                      <MDBCardTitle className="best-seller-name fw-bold">
+                        {value.name}
+                      </MDBCardTitle>
+                    </div>
+                    <h3 className=" text-muted card-category" style={{ textAlign: 'center' }}>
+  {value.category}
+</h3>
+                    <p
+                      className="text-center fw-bolder fs-5"
+                      style={{ color: "#ed2335" }}
+                    >
+                      {handlePrice(value.price)} {/* Use the handlePrice function here */}
+                    </p>
+                  </MDBCardBody>
+                </MDBCard>
+              </MDBCol>
+            ))}
+          </MDBRow>
+        </MDBContainer>
+      </section>
     </>
   );
 };
+
 export default Items;
